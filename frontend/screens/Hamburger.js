b9926c363325b512d70970988bd77ae2202cfc62
eswar <eswar250493@gmail.com>
2024-01-20 14:31:33 +0530

    pet app project

---
+++ frontend/screens/Hamburger.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Hamburger = () => {
+  return (
+    <View>
+      <Text>Hamburger</Text>
+    </View>
+  )
+}
+
+export default Hamburger
+
+const styles = StyleSheet.create({})
